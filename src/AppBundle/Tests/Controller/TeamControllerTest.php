177b075d50a394e37aed2599b7ed50f6efec38b7
Maxim Zhurilo <fumus.27@gmail.com>
2015-11-21 20:25:16 +0200

    added TeamControllerTest

---
+++ src/AppBundle/Tests/Controller/TeamControllerTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fumus
+ * Date: 21.11.15
+ * Time: 20:20
+ */
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+
+class TeamControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/team');
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+
+        $crawler = $client->request('GET', '/team/');
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+    }
+
+
+    /**
+     * @param $code
+     * @param $url
+     * @dataProvider showProvider
+     */
+    public function testShow($code, $url)
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', $url);
+        $this->assertEquals($code, $client->getResponse()->getStatusCode());
+
+    }
+
+    public function showProvider()
+    {
+        return
+            [
+                [200, "/team/ukraine"],
+                [200, "/team/northern_ireland"],
+                [200, "/country/czech-republic"],
+                [404, "/country/_ukraine_"],
+                [404, "/country/england2"],
+                [404, "/country/12"]
+            ];
+    }
+}
