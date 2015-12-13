cde953ada9a86013c049279b1531bc3d046ac95e
Maxim Zhurilo <fumus.27@gmail.com>
2015-12-13 22:03:50 +0200

    added pagination modified tests

--- src/AppBundle/Controller/PlayerController.php
+++ src/AppBundle/Controller/PlayerController.php
@@ -9,7 +9,7 @@
 namespace AppBundle\Controller;
 
 
-use AppBundle\Model\Player;
+use AppBundle\Entity\Player;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -20,15 +20,36 @@ use Symfony\Component\HttpFoundation\Response;
 class PlayerController extends Controller
 {
     /**
-     * @Route("/player")
-     * @Route("/player/")
+     * @Route("/player", name="show_all_players")
      * @Method("GET")
+     * @Template()
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->redirectToRoute("homepage",[], 302);
+        //return $this->redirectToRoute("homepage",[], 302);
+        /*
+         $curPage = $request->query->getInt('page', 1);
+         $limit = 50;
+         $em = $this->getDoctrine()->getManager();
+         $count = $em->getRepository('AppBundle:Player')->countAllPlayers();
+         $users = $em->getRepository('AppBundle:Player')->getAllUsersWithPagination($limit, $curPage);
+         if($count > ($limit*$curPage) ) {
+             $nextPage = $curPage+1;
+         } else $nextPage = false;
+         if ($request->isXmlHttpRequest()) {
+             $content = $this->renderView('AppBundle:Player:body.html.twig',
+                 ['users' => $users , 'nextPage' => $nextPage]);
+             return new Response($content);
+         }
+         return ['users' => $users , 'nextPage' => $nextPage];
+        */
+        $em = $this->getDoctrine()->getManager();
+        $players = $em->getRepository('AppBundle:Player')
+            ->findAllPlayersWithDependencies();
+        $pager = $this->get('knp_paginator');
+        $pagination = $pager->paginate($players, $request->query->getInt('page', 1), 20);
+        return ['players' => $pagination];
     }
-
     /**
      * @Route("/player/{id}", name="show_player", requirements={"id" = "\d+"})
      * @Template()
@@ -36,15 +57,12 @@ class PlayerController extends Controller
      */
     public function showAction($id)
     {
-
         $player = $this->getDoctrine()
             ->getRepository('AppBundle:Player')
             ->find($id);
-
         if(!$player) {
             throw $this->createNotFoundException('No player found for id '.$id);
         }
-
         return ['player' => $player];
     }
 }
--- src/AppBundle/Tests/Controller/PlayerControllerTest.php
+++ src/AppBundle/Tests/Controller/PlayerControllerTest.php
@@ -27,8 +27,6 @@ class PlayerControllerTest extends AbstractController
     {
         return
             [
-                [302, "/player/"],
-                [302, "/player"],
                 [404, "/player/player6"],
                 [404, "/player/player_"],
                 [200, "/player/1"]
