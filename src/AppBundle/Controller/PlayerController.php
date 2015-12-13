b5fb131b0342ceb2e71c471fa8be075ce3b9da05
Maxim Zhurilo <fumus.27@gmail.com>
2015-12-13 21:46:01 +0200

    Revert "added pagination"
    This reverts commit 2fe300339d64b8b96957e1a1ccc614479cf9a629.

--- src/AppBundle/Controller/PlayerController.php
+++ src/AppBundle/Controller/PlayerController.php
@@ -9,7 +9,7 @@
 namespace AppBundle\Controller;
 
 
-use AppBundle\Entity\Player;
+use AppBundle\Model\Player;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -20,42 +20,13 @@ use Symfony\Component\HttpFoundation\Response;
 class PlayerController extends Controller
 {
     /**
-     * @Route("/player", name="show_all_players")
+     * @Route("/player")
+     * @Route("/player/")
      * @Method("GET")
-     * @Template()
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        //return $this->redirectToRoute("homepage",[], 302);
-       /*
-        $curPage = $request->query->getInt('page', 1);
-        $limit = 50;
-        $em = $this->getDoctrine()->getManager();
-        $count = $em->getRepository('AppBundle:Player')->countAllPlayers();
-        $users = $em->getRepository('AppBundle:Player')->getAllUsersWithPagination($limit, $curPage);
-
-        if($count > ($limit*$curPage) ) {
-            $nextPage = $curPage+1;
-        } else $nextPage = false;
-
-        if ($request->isXmlHttpRequest()) {
-            $content = $this->renderView('AppBundle:Player:body.html.twig',
-                ['users' => $users , 'nextPage' => $nextPage]);
-            return new Response($content);
-        }
-        return ['users' => $users , 'nextPage' => $nextPage];
-       */
-
-        $em = $this->getDoctrine()->getManager();
-
-        $players = $em->getRepository('AppBundle:Player')
-            ->findAllPlayersWithDependencies();
-
-        $pager = $this->get('knp_paginator');
-        $pagination = $pager->paginate($players, $request->query->getInt('page', 1), 20);
-
-        return ['players' => $pagination];
-
+        return $this->redirectToRoute("homepage",[], 302);
     }
 
     /**
